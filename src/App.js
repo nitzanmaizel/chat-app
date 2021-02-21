19c64110a8b88f8ecb72f1734d9dee151e89c1e2
unknown <nitzanmaizel@gmail.com>
2021-02-21 22:25:18 +0200

    add styles && add npm packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
+import React from "react"
+import 'rsuite/dist/styles/rsuite-default.css';
+import "./styles/main.scss"
 
 function App() {
   return (
     <div className="App">
+      Hello World!!!
     </div>
   );
 }
