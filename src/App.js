d114022567d5d24ef3e243d0b8a5b762a6f05dc6
unknown <nitzanmaizel@gmail.com>
2021-02-21 23:19:02 +0200

    Create PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { Route, Switch } from "react-router";
 import 'rsuite/dist/styles/rsuite-default.css';
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import "./styles/main.scss"
 
 function App() {
   return (
 <Switch>
-  <Route><SignIn/></Route>
+  <Route path="/signin"><SignIn/></Route>
+  <PrivateRoute path="/">
+    <Home/>
+  </PrivateRoute>
 </Switch>
   );
 }
